86b8bb53e2cfad84a5f353aab5ab1df9d6f770bf
Irwin Romero Rodriguez <irwin.romero.rdz@gmail.com>
2026-01-18 23:10:08 -0600

    feat: Add global authentication status checking and display a full-screen loader during the process.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <RouterView />
+  <FullScreenLoader v-if="authStore.authStatus === AuthStatus.CHECKING" />
+  <RouterView v-else />
   <VueQueryDevtools />
 </template>
 
@@ -8,6 +9,7 @@ import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 import { useRoute, useRouter } from 'vue-router';
 import { AuthStatus } from './modules/auth/interfaces';
 import { useAuthStore } from './modules/auth/stores/auth.store';
+import FullScreenLoader from './modules/common/components/FullScreenLoader.vue';
 
 const authStore = useAuthStore();
 const router = useRouter();
